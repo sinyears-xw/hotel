aa2523eb1d850b187816c504c7929a134c324a4b
xiongwei <xiongwei@gooalgene.com>
2019-11-22 12:35:00 +0800

    weix

--- src/main/java/com/jiuzhe/hotel/service/impl/AccountServiceImpl.java
+++ src/main/java/com/jiuzhe/hotel/service/impl/AccountServiceImpl.java
@@ -18,141 +18,139 @@ import java.util.Map;
 @Service
 public class AccountServiceImpl implements AccountService {
 
-	@Autowired
-	private JdbcTemplate jdbcTemplate;
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
-	@Autowired
-	private StringRedisTemplate rt;
+    @Autowired
+    private StringRedisTemplate rt;
 
-	private  Log logger = LogFactory.getLog(this.getClass());
+    private Log logger = LogFactory.getLog(this.getClass());
 
-	private boolean checkAccount(String id) {
-		Map num = jdbcTemplate.queryForMap("select count(1) num from account where user_id = '" + id + "'");
-		if (Integer.parseInt(num.get("num").toString()) > 0)
-			return true;
-		else
-			return false;
-	}
+    private boolean checkAccount(String id) {
+        Map num = jdbcTemplate.queryForMap("select count(1) num from account where user_id = '" + id + "'");
+        if (Integer.parseInt(num.get("num").toString()) > 0)
+            return true;
+        else
+            return false;
+    }
 
-	private boolean checkChannel(String c) {
-		String[] channels = new String[]{"alipay","wx","wx_pub","wx_lite","bank_account"};
-		for (int i = 0; i < channels.length; i++) {
-			if (c.equals(channels[i]))
-				return true;
-		}
-		return false;
-	}
+    private boolean checkChannel(String c) {
+        String[] channels = new String[]{"alipay", "wx", "wx_pub", "wx_lite", "bank_account"};
+        for (int i = 0; i < channels.length; i++) {
+            if (c.equals(channels[i]))
+                return true;
+        }
+        return false;
+    }
 
-	public Map checkPasswd(Map param) {
-		String userId = param.get("user_id").toString();
-		if (StringUtil.isEmpty(userId))
-			return RtCodeConstant.getResult("40001");
+    public Map checkPasswd(Map param) {
+        String userId = param.get("user_id").toString();
+        if (StringUtil.isEmpty(userId))
+            return RtCodeConstant.getResult("40001");
 
-		Map accountPasswd = null;
-		try {
-			accountPasswd = jdbcTemplate.queryForMap("select passwd from account where user_id = '" + userId + "' for update");
-		} catch (EmptyResultDataAccessException e) {
-			logger.error(e);
-			return RtCodeConstant.getResult("-1");
-		}
+        Map accountPasswd = null;
+        try {
+            accountPasswd = jdbcTemplate.queryForMap("select passwd from account where user_id = '" + userId + "' for update");
+        } catch (EmptyResultDataAccessException e) {
+            logger.error(e);
+            return RtCodeConstant.getResult("-1");
+        }
 
-		String oldPasswd = param.get("old_passwd").toString();
-		if (StringUtil.isEmpty(oldPasswd))
-			return RtCodeConstant.getResult("40001");
+        String oldPasswd = param.get("old_passwd").toString();
+        if (StringUtil.isEmpty(oldPasswd))
+            return RtCodeConstant.getResult("40001");
 
-		String storedPasswd = accountPasswd.get("passwd").toString();
-		if (!storedPasswd.equals(oldPasswd)) {
-			return RtCodeConstant.getResult("10007");
-		}
+        String storedPasswd = accountPasswd.get("passwd").toString();
+        if (!storedPasswd.equals(oldPasswd)) {
+            return RtCodeConstant.getResult("10007");
+        }
 
-		return RtCodeConstant.getResult("0");
-	}
+        return RtCodeConstant.getResult("0");
+    }
 
-	@Transactional
-	public Map delSettleAccount(Map param) {
-		String userId = param.get("user_id").toString();
-		if (StringUtil.isEmpty(userId))
-			return RtCodeConstant.getResult("40001");
+    @Transactional
+    public Map delSettleAccount(Map param) {
+        String userId = param.get("user_id").toString();
+        if (StringUtil.isEmpty(userId))
+            return RtCodeConstant.getResult("40001");
 
-		jdbcTemplate.update(String.format("UPDATE account SET aliAccount = null, aliName = null where user_id='%s' ",userId));
+        jdbcTemplate.update(String.format("UPDATE account SET aliAccount = null, aliName = null where user_id='%s' ", userId));
 
-		return RtCodeConstant.getResult("0");
-	}		
+        return RtCodeConstant.getResult("0");
+    }
 
-	@Transactional
-	public Map saveSettleAccount(Map param) {
-		String userId = param.get("user_id").toString();
-		if (StringUtil.isEmpty(userId))
-			return RtCodeConstant.getResult("40001");
-		if (!checkAccount(userId))
-			return RtCodeConstant.getResult("30006");
+    @Transactional
+    public Map saveSettleAccount(Map param) {
+        String userId = param.get("user_id").toString();
+        if (StringUtil.isEmpty(userId))
+            return RtCodeConstant.getResult("40001");
+        if (!checkAccount(userId))
+            return RtCodeConstant.getResult("30006");
 
-		String channel = param.get("channel").toString();
-		if (StringUtil.isEmpty(channel) || !checkChannel(channel))
-			return RtCodeConstant.getResult("40001");
+        String channel = param.get("channel").toString();
+        if (StringUtil.isEmpty(channel) || !checkChannel(channel))
+            return RtCodeConstant.getResult("40001");
 
-		String recipient_account = param.get("recipient_account").toString();
-		if (StringUtil.isEmpty(recipient_account))
-			return RtCodeConstant.getResult("40001");
+        String recipient_account = param.get("recipient_account").toString();
+        if (StringUtil.isEmpty(recipient_account))
+            return RtCodeConstant.getResult("40001");
 
-		String recipient_name = param.get("recipient_name").toString();
-		if (StringUtil.isEmpty(recipient_name))
-			return RtCodeConstant.getResult("40001");
+        String recipient_name = param.get("recipient_name").toString();
+        if (StringUtil.isEmpty(recipient_name))
+            return RtCodeConstant.getResult("40001");
 
-		switch (channel) {
-			case "alipay":
-				jdbcTemplate.update(String.format("update account set aliAccount='%s',aliName='%s' where user_id='%s'", recipient_account, recipient_name, userId, channel));
-				break;
-		}
-		return RtCodeConstant.getResult("0");
-	}		
-
-
-	@Transactional
-	public Map updatePasswd(Map param) {
-		String userId = param.get("user_id").toString();
-		if (StringUtil.isEmpty(userId))
-			return RtCodeConstant.getResult("40001");
-
-		Map accountPasswd = null;
-		try {
-			accountPasswd = jdbcTemplate.queryForMap("select passwd from account where user_id = '" + userId + "' for update");
-		} catch (EmptyResultDataAccessException e) {
-			logger.error(e);
-			return RtCodeConstant.getResult("-1");
-		}
-
-		String oldPasswd = param.get("old_passwd").toString();
-		if (StringUtil.isEmpty(oldPasswd))
-			return RtCodeConstant.getResult("40001");
-
-		String newPasswd = param.get("new_passwd").toString();
-		if (StringUtil.isEmpty(newPasswd))
-			return RtCodeConstant.getResult("40001");
-
-		String storedPasswd = accountPasswd.get("passwd").toString();
-		if (storedPasswd.equals(oldPasswd)) {
-			jdbcTemplate.update(String.format("update account set passwd = '%s', updt = now() where user_id = '%s'",newPasswd,userId));
-			return RtCodeConstant.getResult("0");
-		}
-		
-		return RtCodeConstant.getResult("-1");
-	}
-
-	public Map getAccountInfo(String id) {
-		try {
-			Map account = jdbcTemplate.queryForMap("select total_balance,disable,available_balance from account where user_id = '" + id + "'");
-			return RtCodeConstant.getResult("0", account);
-		} catch (EmptyResultDataAccessException e) {
-			return RtCodeConstant.getResult("-1");
-		} 
-	}
+        switch (channel) {
+            case "alipay":
+                jdbcTemplate.update(String.format("update account set aliAccount='%s',aliName='%s' where user_id='%s'", recipient_account, recipient_name, userId, channel));
+                break;
+        }
+        return RtCodeConstant.getResult("0");
+    }
 
 
-	public Map getSettleAccount(String id) {
-		String sql = String.format("select aliAccount, aliName from account where user_id = '%s' ",id);
-		List<Map<String, Object>> products = jdbcTemplate.queryForList(sql);
-		return RtCodeConstant.getResult("0", products);
-	}
+    @Transactional
+    public Map updatePasswd(Map param) {
+        String userId = param.get("user_id").toString();
+        String oldPasswd = "";
+
+        if (StringUtil.isEmpty(userId))
+            return RtCodeConstant.getResult("40001");
+
+        String newPasswd = param.get("new_passwd").toString();
+        if (StringUtil.isEmpty(newPasswd))
+            return RtCodeConstant.getResult("40001");
+
+        Map accountPasswd = jdbcTemplate.queryForMap("select passwd from account where user_id = '" + userId + "' for update");
+        if (accountPasswd == null || accountPasswd.size() == 0) {
+
+        } else {
+            oldPasswd = param.get("old_passwd").toString();
+            if (StringUtil.isEmpty(oldPasswd))
+                return RtCodeConstant.getResult("40001");
+
+            String storedPasswd = accountPasswd.get("passwd").toString();
+            if (!storedPasswd.equals(oldPasswd))
+                return RtCodeConstant.getResult("10007");
+        }
+
+        jdbcTemplate.update(String.format("update account set passwd = '%s', updt = now() where user_id = '%s'", newPasswd, userId));
+        return RtCodeConstant.getResult("0");
+    }
+
+    public Map getAccountInfo(String id) {
+        try {
+            Map account = jdbcTemplate.queryForMap("select total_balance,disable,available_balance from account where user_id = '" + id + "'");
+            return RtCodeConstant.getResult("0", account);
+        } catch (EmptyResultDataAccessException e) {
+            return RtCodeConstant.getResult("-1");
+        }
+    }
+
+
+    public Map getSettleAccount(String id) {
+        String sql = String.format("select aliAccount, aliName from account where user_id = '%s' ", id);
+        List<Map<String, Object>> products = jdbcTemplate.queryForList(sql);
+        return RtCodeConstant.getResult("0", products);
+    }
 }
 
